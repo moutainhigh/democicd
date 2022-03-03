755afb78454d675958e6ab4e542815c703673cc6
caisj <328132024@qq.com>
2022-03-03 14:44:18 +0800

    add

--- src/main/java/com/fishpro/helloworld/controller/index.java
+++ src/main/java/com/fishpro/helloworld/controller/index.java
@@ -1,14 +1,13 @@
 package com.fishpro.helloworld.controller;
 
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
 
-@Controller
-@RequestMapping("/app")
-public class TestController {
+@RestController
+public class index {
 
-    @RequestMapping("/test")
+    @RequestMapping ("/test")
     @ResponseBody
     public String testDemo() {
         return "Hello World!";
